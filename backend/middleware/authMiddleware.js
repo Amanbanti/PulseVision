82e5d7f6bb99ebe74c3b5a28b118829e46ad1caf
Amanbanti <amanbanti2011@gmail.com>
2025-07-16 13:35:34 +0300

    auth Middleware commit

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -28,7 +28,7 @@ export const protect = asyncHandler( async(req,res,next) =>{
 
 //Admin Middleware
 export const admin = (req,res,next) => {
-    if (req.user && req.user.isAdmin){
+    if (req.user && req.user.role === 'admin') {
         next()
     }else{
         res.status(401);
